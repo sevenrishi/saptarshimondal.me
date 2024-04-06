6765d385dbd3614ad16f1e8cd2c2aa2dc82799c7
Saptarshi <saptarshimondal768@gmail.com>
2024-04-07 03:24:53 +0530

    many changes

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -39,7 +39,7 @@
   }
 
   // Descriptions of all options available on the demo site:
-  // http://lokeshdhakar.com/projects/lightbox2/index.html#options
+  // http://lokeshdhakar.com/projects/lightbox/#options
   Lightbox.defaults = {
     albumLabel: 'Image %1 of %2',
     alwaysShowNavOnTouchDevices: false,
